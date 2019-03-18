01f00866ff6869477c1c0289f45c70d47b4c1cb4
wgrey <lu036@yandex.ru>
2019-03-18 18:08:36 +0300

    Initial commit

---
+++ src/main/java/com/wgrey/printstatistics/PrintstatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.wgrey.printstatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintstatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintstatisticsApplication.class, args);
+	}
+
+}
